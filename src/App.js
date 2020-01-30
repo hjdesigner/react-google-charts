25d9f0a5e3bb9898d7e50f1487be50302a14c2fb
Henrique Melanda <henrique.melanda@vagas.com.br>
2020-01-30 20:19:40 -0300

    Criado o primeiro grafico de pizza

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Chart } from "react-google-charts";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [options, setOptions] = useState({
+    title: 'Gráfico de Pizza'
+  })
+  const [data, setData] = useState([
+    ['Linguagens', 'Quantidade'],
+    ['React', 100],
+    ['Angula', 80],
+    ['Vue', 50],
+  ])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Chart
+            width={'500px'}
+            height={'300px'}
+            chartType="PieChart"
+            data={data}
+            options={options}
+          />
+        </div>
       </header>
     </div>
   );
