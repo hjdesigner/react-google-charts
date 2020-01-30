50d7f9446f2c43606fc8f0d7d29a25cbf37ead0f
Henrique Melanda <henrique.melanda@vagas.com.br>
2020-01-30 20:53:24 -0300

    Criado o grafico de barra

--- src/App.js
+++ src/App.js
@@ -6,24 +6,42 @@ import './App.css';
 function App() {
   const [options, setOptions] = useState({
     title: 'Gráfico de Pizza'
-  })
+  });
+  const [optionsBar, setOptionsBar] = useState({
+    title: 'Gráfico de Barra'
+  });
   const [data, setData] = useState([
     ['Linguagens', 'Quantidade'],
     ['React', 100],
     ['Angula', 80],
     ['Vue', 50],
+  ]);
+  const [dataBar, setDataBar] = useState([
+    ['Cidades', '2010 População', '2000 População'],
+    ['New York City, NY', 8175000, 8008000],
+    ['Los Angeles, CA', 3792000, 3694000],
+    ['Chicago, IL', 2695000, 2896000],
+    ['Houston, TX', 2099000, 1953000],
+    ['Philadelphia, PA', 1526000, 1517000],
   ])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>
+        <div style={{display: "flex"}}>
           <Chart
             width={'500px'}
             height={'300px'}
             chartType="PieChart"
             data={data}
             options={options}
+          />
+          <Chart
+            width={'500px'}
+            height={'300px'}
+            chartType="BarChart"
+            data={dataBar}
+            options={optionsBar}
           />
         </div>
       </header>
